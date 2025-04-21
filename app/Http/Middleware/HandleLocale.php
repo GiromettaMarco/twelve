48e062c98e3fffef2d738d529148855a0e9f7027
Marco Girometta <girometta.marco@gmail.com>
2025-04-21 20:56:30 +0200

    fix(server): improve locale inference

--- app/Http/Middleware/HandleLocale.php
+++ app/Http/Middleware/HandleLocale.php
@@ -10,24 +10,52 @@ use Symfony\Component\HttpFoundation\Response;
 class HandleLocale
 {
     /**
+     * Available languages.
+     */
+    public $available = [
+        'en',
+        'it',
+    ];
+
+    /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $locale = substr($request->header('Accept-Language', 'en'), 0, 2);
+        $acceptLanguage = explode(',', trim($request->header('Accept-Language', '*')));
+        $parsedLanguage = [];
 
-        if (
-            in_array(
-                $locale,
-                [
-                    'en',
-                    'it',
-                ]
-            )
-        ) {
-            App::setLocale($locale);
+        foreach ($acceptLanguage as $qualityLang) {
+            $qLangArr = array_map('trim', explode(';', trim($qualityLang)));
+            $q = 1;
+
+            if (isset($qLangArr[1]) && substr($qLangArr[1], 0, 2) === 'q=') {
+                $q = (float)substr($qLangArr[1], 2);
+            }
+
+            $parsedLanguage[] = ['q' => $q, 'lang' => $qLangArr[0]];
+
+            usort($parsedLanguage, function ($a, $b) {
+                return ($a < $b);
+            });
+        }
+
+        foreach($parsedLanguage as $lang) {
+            if (in_array($lang['lang'], $this->available)) {
+                App::setLocale($lang['lang']);
+                return $next($request);
+            }
+
+            if (str_contains($lang['lang'], '-')) {
+                $noCountryLang = explode('-', $lang['lang'])[0];
+
+                if (in_array($noCountryLang, $this->available)) {
+                    App::setLocale($noCountryLang);
+                    return $next($request);
+                }
+            }
         }
 
         return $next($request);
