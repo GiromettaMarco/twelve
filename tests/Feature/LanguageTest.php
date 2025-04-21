8522737b7044f7f2ae3af604f546d58de4c4ca36
Marco Girometta <girometta.marco@gmail.com>
2025-04-21 21:05:03 +0200

    test(server): update language test

--- tests/Feature/LanguageTest.php
+++ tests/Feature/LanguageTest.php
@@ -9,7 +9,7 @@ class LanguageTest extends TestCase
     public function test_known_language(): void
     {
         $response = $this->withHeaders([
-            'HTTP_ACCEPT_LANGUAGE' => 'it',
+            'HTTP_ACCEPT_LANGUAGE' => 'it-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5',
         ])->get('/');
 
         $response->assertSee('<html lang="it"', false);
@@ -18,8 +18,24 @@ class LanguageTest extends TestCase
     public function test_unknown_language(): void
     {
         $response = $this->withHeaders([
-            'HTTP_ACCEPT_LANGUAGE' => 'es',
+            'HTTP_ACCEPT_LANGUAGE' => 'fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5',
         ])->get('/');
+
+        $response->assertSee('<html lang="en"', false);
+    }
+
+    public function test_any_language(): void
+    {
+        $response = $this->withHeaders([
+            'HTTP_ACCEPT_LANGUAGE' => '*',
+        ])->get('/');
+
+        $response->assertSee('<html lang="en"', false);
+    }
+
+    public function test_no_header(): void
+    {
+        $response = $this->get('/');
 
         $response->assertSee('<html lang="en"', false);
     }
