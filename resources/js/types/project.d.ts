e346a7acb39b1e6771896acc81c7b6b1aa8abd3b
Marco Girometta <girometta.marco@gmail.com>
2025-04-28 04:16:34 +0200

    fix(client): fix project typing

--- resources/js/types/project.d.ts
+++ resources/js/types/project.d.ts
@@ -3,7 +3,9 @@ import type { Task } from '@/types/task'
 export interface Project {
   id: number
   title: string
+  text?: string
   archived: boolean
+  deadline?: string
   created_at: string
   updated_at: string
   tasks: Task[]
