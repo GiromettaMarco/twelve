662465d75421d78152a649c4935705cf3593c1c2
Marco Girometta <girometta.marco@gmail.com>
2025-05-21 22:30:30 +0200

    feat(client): add hydration in case of ssr

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,7 +3,7 @@ import '../css/app.css'
 import { createInertiaApp } from '@inertiajs/react'
 import { LaravelReactI18nProvider } from 'laravel-react-i18n'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { initializeTheme } from './hooks/use-appearance'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
@@ -12,13 +12,24 @@ createInertiaApp({
   title: (title) => `${title} - ${appName}`,
   resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
   setup({ el, App, props }) {
-    const root = createRoot(el)
+    if (import.meta.env.SSR) {
+      // SSR
+      hydrateRoot(
+        el,
+        <LaravelReactI18nProvider fallbackLocale={'en'} files={import.meta.glob('/lang/*.json')}>
+          <App {...props} />
+        </LaravelReactI18nProvider>
+      )
+    } else {
+      // CSR
+      const root = createRoot(el)
 
-    root.render(
-      <LaravelReactI18nProvider fallbackLocale={'en'} files={import.meta.glob('/lang/*.json')}>
-        <App {...props} />
-      </LaravelReactI18nProvider>
-    )
+      root.render(
+        <LaravelReactI18nProvider fallbackLocale={'en'} files={import.meta.glob('/lang/*.json')}>
+          <App {...props} />
+        </LaravelReactI18nProvider>
+      )
+    }
   },
   progress: {
     color: '#4B5563'
