9031fad7f07dc5010a4ccfbb891909200a29abf1
Marco Girometta <girometta.marco@gmail.com>
2025-05-24 04:26:03 +0200

    fix(client): define task columns in a custom hook and add localization

---
+++ resources/js/components/tasks/use-task-columns.tsx
@@ -0,0 +1,131 @@
+import { labels, priorities, statuses } from '@/components/tasks/data'
+import { Badge } from '@/components/ui/badge'
+import { Checkbox } from '@/components/ui/checkbox'
+import type { Task } from '@/types/task'
+import { ColumnDef } from '@tanstack/react-table'
+import { useLaravelReactI18n } from 'laravel-react-i18n'
+import { useState } from 'react'
+import { DataTableColumnHeader } from './data-table-column-header'
+import { DataTableRowActions } from './data-table-row-actions'
+
+export function useTaskColumns() {
+  // Setup translations
+  const { t } = useLaravelReactI18n()
+
+  // const [columns, setColumns] = useState(...)
+  const [columnDef] = useState<ColumnDef<Task>[]>([
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label={t('Select all')}
+          className="block"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label={t('Select row')}
+          className="block"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false
+    },
+    {
+      // @TODO replace with position
+      accessorKey: 'id',
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={t('#')}
+        />
+      ),
+      cell: ({ row }) => <div>{row.getValue('id')}</div>,
+      enableSorting: true,
+      enableHiding: true
+    },
+    {
+      accessorKey: 'title',
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={t('Title')}
+        />
+      ),
+      cell: ({ row }) => {
+        const label = labels.find((label) => label.value === row.original.label.title)
+
+        return (
+          <div className="flex space-x-2">
+            {label && <Badge variant="outline">{label.label}</Badge>}
+            <span className="truncate font-medium">{row.getValue('title')}</span>
+          </div>
+        )
+      }
+    },
+    {
+      accessorKey: 'status',
+      accessorFn: (row) => row.status.title,
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={t('Status')}
+        />
+      ),
+      cell: ({ row }) => {
+        const status = statuses.find((status) => status.value === row.getValue('status'))
+
+        if (!status) {
+          return null
+        }
+
+        return (
+          <div className="flex items-center">
+            {status.icon && <status.icon className="text-muted-foreground mr-2 h-4 w-4" />}
+            <span>{status.label}</span>
+          </div>
+        )
+      },
+      filterFn: (row, id, value) => {
+        return value.includes(row.getValue(id))
+      }
+    },
+    {
+      accessorKey: 'priority',
+      accessorFn: (row) => row.priority.title,
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={t('Priority')}
+        />
+      ),
+      cell: ({ row }) => {
+        const priority = priorities.find((priority) => priority.value === row.getValue('priority'))
+
+        if (!priority) {
+          return null
+        }
+
+        return (
+          <div className="flex items-center">
+            {priority.icon && <priority.icon className="text-muted-foreground mr-2 h-4 w-4" />}
+            <span>{priority.label}</span>
+          </div>
+        )
+      },
+      filterFn: (row, id, value) => {
+        return value.includes(row.getValue(id))
+      }
+    },
+    {
+      id: 'actions',
+      cell: ({ row }) => <DataTableRowActions row={row} />
+    }
+  ])
+
+  return { columnDef }
+}
