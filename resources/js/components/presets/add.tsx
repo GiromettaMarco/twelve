b6899b0d8acebf7478db17fdc741f9a7d44a0345
Marco Girometta <girometta.marco@gmail.com>
2025-06-04 17:07:01 +0200

    fix: set default aria label to the add component

--- resources/js/components/presets/add.tsx
+++ resources/js/components/presets/add.tsx
@@ -1,17 +1,23 @@
 import { Button } from '@/components/ui/button'
+import { useLaravelReactI18n } from 'laravel-react-i18n'
 import { Plus } from 'lucide-react'
 import type { ComponentProps } from 'react'
 
 type AddProps = ComponentProps<'button'> & {
+  /** Optional label of the button */
   label?: string
 }
 
+/** HTML button to create new resources */
 export function Add({ label, ...props }: AddProps) {
+  // Setup translations
+  const { t } = useLaravelReactI18n()
+
   return (
     <Button
       size="sm"
       className="h-7"
-      aria-label={label}
+      aria-label={label ?? t('Add')}
       {...props}
     >
       <Plus />
