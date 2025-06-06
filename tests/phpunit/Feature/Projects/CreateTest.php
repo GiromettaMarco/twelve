36d348f4b7ec268b062314b279f72996bbaab31e
Marco Girometta <girometta.marco@gmail.com>
2025-06-06 11:17:07 +0200

    test(server): add new tests

---
+++ tests/phpunit/Feature/Projects/CreateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\PhpUnit\Feature\Projects;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\PhpUnit\TestCase;
+
+class CreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_users_can_create_project()
+    {
+        $this->actingAs(User::first());
+
+        $this
+            ->followingRedirects()
+            ->post('/dashboard/projects', [
+                'title' => 'New project',
+                'description' => 'unique string 753951',
+                'deadline' => '2025-12-31'
+            ])
+            ->assertOk();
+
+        $project = Project::where('description', 'unique string 753951')->first();
+
+        $this->assertTrue($project?->title === 'New project');
+    }
+}
---
+++ tests/phpunit/Feature/Projects/DeleteTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\PhpUnit\Feature\Projects;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Tests\PhpUnit\TestCase;
+
+class DeleteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_delete_project()
+    {
+        $this->actingAs(User::first());
+
+        $this
+            ->followingRedirects()
+            ->delete('/dashboard/projects/1')->assertInertia(
+                fn(Assert $page) => $page
+                    ->has(
+                        'flash',
+                        fn(Assert $page) => $page
+                            ->where('title', 'Project deleted')
+                            ->etc()
+                    )
+            );
+
+        $this->get('/dashboard/projects/1')->assertNotFound();
+    }
+}
