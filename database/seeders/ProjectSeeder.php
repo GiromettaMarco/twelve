2e358026a128c93e30d69c1af5a2d40d9dcc8e1d
Marco Girometta <girometta.marco@gmail.com>
2025-06-28 05:02:27 +0200

    fix(server): update project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -14,18 +14,33 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        $project = Project::create([
+        $project1 = Project::create([
             'title' => 'Work in Progress',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis eu augue eget congue. Praesent blandit neque justo. Morbi scelerisque mollis metus, vel accumsan ex malesuada id.',
             'deadline' => Carbon::tomorrow(),
         ]);
 
+        $project2 = Project::create([
+            'title' => 'Project Test',
+            'description' => 'Test project reorder by dragging this project.',
+            'deadline' => Carbon::today()->addDays(7),
+        ]);
+
         $user = User::find(1);
+
         if ($user) {
-            $project->users()->attach(
+            $project1->users()->attach(
                 $user,
                 [
                     'position' => 0,
+                    'role' => 'admin',
+                ]
+            );
+
+            $project2->users()->attach(
+                $user,
+                [
+                    'position' => 1,
                     'role' => 'admin',
                 ]
             );
