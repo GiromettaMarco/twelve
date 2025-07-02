35df2e0530f798e07dde494ff52a31b03e6ff867
Marco Girometta <girometta.marco@gmail.com>
2025-07-02 23:35:37 +0200

    docs(client): update docs

--- resources/js/providers/appearance-context.tsx
+++ resources/js/providers/appearance-context.tsx
@@ -1,7 +1,7 @@
 import type { Appearance, Theme } from '@/lib/appearance'
 import { createContext } from 'react'
 
-// @BUG react context api doesn't work with inertia
+// @NOTE react context api doesn't work with inertia
 // useContext returns default values in the page component
 
 type AppearanceContextType = {
