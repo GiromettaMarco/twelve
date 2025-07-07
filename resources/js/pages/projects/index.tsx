e017cf2c0b7a743ad7ba1c4527e2615781743438
Marco Girometta <girometta.marco@gmail.com>
2025-07-07 07:29:15 +0200

    chore(client): remove placeholder

--- resources/js/pages/projects/index.tsx
+++ resources/js/pages/projects/index.tsx
@@ -1,6 +1,5 @@
 import AddProject from '@/components/projects/add-project'
 import ProjectsBoard from '@/components/projects/projects-board'
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern'
 import AppLayout from '@/layouts/app-layout'
 import { type BreadcrumbItem } from '@/types'
 import type { Project } from '@/types/project'
@@ -28,12 +27,9 @@ export default function Projects({ projects = [] }: { projects: Project[] }) {
       headerChildren={AddProject()}
     >
       <Head title={tChoice('Project', 2)} />
+
       <div className="flex h-full flex-1 flex-col gap-4 rounded-xl px-4 md:px-0">
         <ProjectsBoard projects={projects} />
-
-        <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-          <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-        </div>
       </div>
     </AppLayout>
   )
